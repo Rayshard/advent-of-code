c5a0aa9f6424ad5e70c92b8b489f32073660acc0
Rayshard Thompson <rayshard.thompson@kensho.com>
2022-12-03 13:03:12 -0500

    day 3 - part 2

--- 2022/day3/src/main.rs
+++ 2022/day3/src/main.rs
@@ -4,31 +4,60 @@ use std::{
     io::{self, BufRead, BufReader},
 };
 
-fn main() -> io::Result<()> {
-    let file = File::open("input.txt")?;
-    let reader = BufReader::new(file);
+fn get_priority(c: char) -> u32 {
+    match c as u32 {
+        upper if upper >= 65 && upper <= 90 => upper - 64 + 26,
+        lower if lower >= 97 && lower <= 122 => lower - 96,
+        value => panic!("Invalid char: {value}"),
+    }
+}
+
+fn get_part1(lines: &Vec<String>) -> u32 {
     let mut total = 0u32;
 
-    for line in reader.lines() {
-        let text = line?;
-        let compartment_size = text.len() / 2;
-        let (compartment1, compartment2) = (&text[..compartment_size], &text[compartment_size..]);
+    for line in lines {
+        let compartment_size = line.len() / 2;
+        let (compartment1, compartment2) = (&line[..compartment_size], &line[compartment_size..]);
         let seen = HashSet::<char>::from_iter(compartment1.chars());
 
         for c in compartment2.chars() {
             if seen.contains(&c) {
-                total += match c as u32 {
-                    upper if upper >= 65 && upper <= 90  => upper - 64 + 26,
-                    lower if lower >= 97 && lower <= 122  => lower - 96,
-                    value => panic!("Invalid char: {value}")
-                };
-
+                total += get_priority(c);
                 break;
             }
         }
     }
 
-    println!("{total}");
+    return total;
+}
+
+fn get_part2(lines: &Vec<String>) -> u32 {
+    let mut total = 0u32;
+    let chunks = lines.chunks(3);
+
+    for chunk in chunks {
+        let elves: Vec<HashSet<_>> = chunk
+            .iter()
+            .map(|line| HashSet::<char>::from_iter(line.chars()))
+            .collect();
+
+        let common : HashSet<char> = elves[0].intersection(&elves[1]).map(|c| *c).collect();
+        let common : HashSet<char> = common.intersection(&elves[2]).map(|c| *c).collect();
+        total += common.iter().map(|c| get_priority(*c)).sum::<u32>();
+    }
+
+    return total;
+}
+
+fn main() -> io::Result<()> {
+    let file = File::open("input.txt")?;
+    let lines = BufReader::new(file)
+        .lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect();
+
+    println!("{}", get_part1(&lines));
+    println!("{}", get_part2(&lines));
 
     Ok(())
 }
