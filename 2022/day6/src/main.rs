de9ee97cfbd8dbf5bf431974e048a4bd10d456ae
Rayshard Thompson <rayshard.thompson@kensho.com>
2022-12-07 09:04:46 -0500

    day 6 - part 2

--- 2022/day6/src/main.rs
+++ 2022/day6/src/main.rs
@@ -1,10 +1,10 @@
 use std::{collections::HashSet, fs, io};
 
-fn get_part1(input: &Vec<char>) -> usize {
-    for i in 0..(input.len() - 3) {
-        let set: HashSet<&char> = HashSet::from_iter(&input[i..i + 4]);
-        if set.len() == 4 {
-            return i + 4;
+fn num_read_b4_all_distinct(input: &Vec<char>, size: usize) -> usize {
+    for i in 0..(input.len() - (size - 1)) {
+        let set: HashSet<&char> = HashSet::from_iter(&input[i..i + size]);
+        if set.len() == size {
+            return i + size;
         }
     }
 
@@ -14,7 +14,9 @@ fn get_part1(input: &Vec<char>) -> usize {
 fn main() -> io::Result<()> {
     let input = fs::read_to_string("input.txt")?.chars().collect::<Vec<_>>();
 
-
-    println!("{}", get_part1(&input));
+    let start_of_packet = num_read_b4_all_distinct(&input, 4);
+    let start_of_message = num_read_b4_all_distinct(&input, 14);
+    println!("{}", start_of_packet);
+    println!("{}", start_of_message);
     Ok(())
 }
