2e581f4ea62e872664b219d6671e7a8aa2b81a11
Rayshard Thompson <rayshard.thompson@kensho.com>
2022-12-08 13:05:25 -0500

    day 8 - part 2

--- 2022/day8/src/main.rs
+++ 2022/day8/src/main.rs
@@ -7,17 +7,73 @@ use std::{
 fn get_visible(strip: &[(usize, u32)]) -> Vec<usize> {
     let mut visible: Vec<usize> = vec![(*strip.first().unwrap()).0, (*strip.last().unwrap()).0];
 
-    for (i_strip, (i_map, height)) in strip.iter().enumerate() {
-        let heights = strip.iter().map(|(_, h)| *h).collect::<Vec<_>>();
-        let before_max = heights[..i_strip].iter().max().unwrap_or(&0);
-        let after_max = heights[i_strip + 1..].iter().max().unwrap_or(&0);
+    // forward
+    let mut tallest = 0u32;
 
-        if before_max < height || after_max < height {
+    for (i_map, height) in strip {
+        if height > &tallest {
+            tallest = *height;
+            visible.push(*i_map);
+        }
+    }
+
+    // reverse
+    tallest = 0u32;
+
+    for (i_map, height) in strip.iter().rev() {
+        if height > &tallest {
+            tallest = *height;
             visible.push(*i_map);
         }
     }
 
     return visible;
+}
+
+fn get_sceneic_score(
+    position: (usize, usize),
+    rows: &[&[(usize, u32)]],
+    cols: &Vec<Vec<(usize, u32)>>,
+) -> u32 {
+    let tree_height = &rows[position.0][position.1].1;
+    let mut from_left = 0u32;
+    let mut from_right = 0u32;
+    let mut from_top = 0u32;
+    let mut from_bottom = 0u32;
+
+    for (_, h) in rows[position.0][..position.1].iter().rev() {
+        from_left += 1;
+
+        if h >= tree_height {
+            break;
+        }
+    }
+
+    for (_, h) in rows[position.0][position.1 + 1..].iter() {
+        from_right += 1;
+
+        if h >= tree_height {
+            break;
+        }
+    } 
+    
+    for (_, h) in cols[position.1][..position.0].iter().rev() {
+        from_top += 1;
+
+        if h >= tree_height {
+            break;
+        }
+    }
+
+    for (_, h) in cols[position.1][position.0 + 1..].iter() {
+        from_bottom += 1;
+
+        if h >= tree_height {
+            break;
+        }
+    }
+    
+    return (from_left * from_right * from_top * from_bottom) as u32;
 }
 
 fn main() -> io::Result<()> {
@@ -34,6 +90,7 @@ fn main() -> io::Result<()> {
     // I'm making the terrible assumption that the input is a square
     let size = (input.len() as f64).sqrt() as usize;
 
+    // Construct rows and columns
     let rows = input.chunks(size).collect::<Vec<_>>();
     let cols = (0..size)
         .map(|i| {
@@ -43,23 +100,27 @@ fn main() -> io::Result<()> {
         })
         .collect::<Vec<_>>();
 
-    // Contains the indices that were seen
+    // Part 1
     let mut visible: HashSet<usize> = HashSet::new();
 
-    for row in rows {
+    for row in rows.iter() {
         visible.extend(get_visible(row));
     }
 
-    for col in cols {
+    for col in cols.iter() {
         visible.extend(get_visible(&col));
     }
 
-    let num_visible = visible.len();
-    println!("{num_visible:?}");
+    println!("{:?}", visible.len());
 
-    let x = get_visible(&[(0, 6), (1, 7), (2, 5), (3, 3), (4, 5)]);
-    let h: HashSet<&usize> = HashSet::from_iter(x.iter());
-    println!("{:?}", h);
+    // Part 2
+    let max_scenic_score = (0..size)
+        .flat_map(|i_row| (0..size).map(move |i_col| (i_row, i_col)))
+        .map(|pos| get_sceneic_score(pos, &rows, &cols))
+        .max()
+        .unwrap_or(0);
+
+    println!("{max_scenic_score}");
 
     Ok(())
 }
