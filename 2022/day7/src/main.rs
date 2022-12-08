2fc439bd4f072ed44790ce537449b1c3791c8309
Rayshard Thompson <rayshard.thompson@kensho.com>
2022-12-08 00:48:33 -0500

    day 7 - part 1

--- 2022/day7/src/main.rs
+++ 2022/day7/src/main.rs
@@ -1,35 +1,167 @@
 use std::{
+    collections::{HashMap, HashSet},
     fs::File,
-    io::{self, BufRead, BufReader}, collections::HashMap,
+    io::{self, BufRead, BufReader},
 };
 
-#[derive(Debug)]
+type FSID = u64;
+
+#[derive(Debug, Clone)]
 struct AOCFile {
     name: String,
     size: u64,
+    parent: FSID,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct AOCDirectory {
     name: String,
-    elements: HashMap<String, Box<FSElement>>,
-    parent: Option<Box<AOCDirectory>>,
+    elements: HashSet<FSID>,
+    parent: Option<FSID>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum FSElement {
     Directory(AOCDirectory),
     File(AOCFile),
 }
 
-fn print_directory(dir: &AOCDirectory, indent: usize) {
-    println!("{:indent$}- {} (dir)", "", dir.name, indent=indent);
+struct FileSystem {
+    elements: HashMap<FSID, FSElement>,
+}
 
-    for element in dir.elements.values() {
-        match element.as_ref() {
-            FSElement::Directory(d) => print_directory(d, indent + 4),
-            FSElement::File(f) =>     println!("{:indent$}- {} (file, size={})", "", f.name, f.size, indent=indent),
-            e => panic!("Not handled: {e:?}")
+impl FileSystem {
+    pub fn new() -> FileSystem {
+        let root = AOCDirectory {
+            name: String::from("/"),
+            elements: HashSet::new(),
+            parent: None,
+        };
+
+        FileSystem {
+            elements: HashMap::from([(0, FSElement::Directory(root))]),
+        }
+    }
+
+    pub fn create(&mut self, element: FSElement) -> FSID {
+        let id = self.elements.len() as FSID;
+
+        match element.clone() {
+            FSElement::Directory(d) => {
+                self.elements.insert(id, element);
+
+                if let Some(FSElement::Directory(parent)) =
+                    self.elements.get_mut(&d.parent.unwrap())
+                {
+                    parent.elements.insert(id);
+                }
+            }
+            FSElement::File(f) => {
+                self.elements.insert(id, element);
+
+                if let Some(FSElement::Directory(parent)) = self.elements.get_mut(&f.parent) {
+                    parent.elements.insert(id);
+                }
+            }
+        };
+
+        return id;
+    }
+
+    pub fn get(&self, id: FSID) -> &FSElement {
+        self.elements.get(&id).unwrap()
+    }
+
+    pub fn get_parent_id(&self, element_id: FSID) -> FSID {
+        match self.get(element_id) {
+            FSElement::Directory(d) => d.parent.unwrap_or(0),
+            FSElement::File(f) => f.parent,
+        }
+    }
+
+    pub fn get_size(&self, element_id: FSID) -> u64 {
+        match self.get(element_id) {
+            FSElement::Directory(d) => d.elements.iter().map(|id| self.get_size(*id)).sum(),
+            FSElement::File(f) => f.size,
+        }
+    }
+
+    pub fn is_directory(&self, fsid: FSID) -> bool {
+        match self.get(fsid) {
+            FSElement::Directory(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn get_directory(&self, fsid: FSID) -> &AOCDirectory {
+        match self.get(fsid) {
+            FSElement::Directory(d) => d,
+            other => panic!("Not a directory: {other:?}"),
+        }
+    }
+
+    pub fn get_all_directories(&self) -> Vec<(FSID, &AOCDirectory)> {
+        self.elements.keys()
+            .filter(|id| self.is_directory(**id))
+            .map(|id| (*id, self.get_directory(*id)))
+            .collect()
+    }
+
+    pub fn get_subdirectories(&self, parent: FSID) -> Vec<(FSID, &AOCDirectory)> {
+        self.get_directory(parent)
+            .elements
+            .iter()
+            .filter(|id| self.is_directory(**id))
+            .map(|id| (*id, self.get_directory(*id)))
+            .collect()
+    }
+
+    pub fn get_name(&self, fsid: FSID) -> &str {
+        match self.get(fsid) {
+            FSElement::Directory(d) => &d.name,
+            FSElement::File(f) => &f.name,
+        }
+    }
+
+    pub fn has_subdirectory(&self, parent_id: FSID, subdir_name: &str) -> bool {
+        for (_, subdir) in self.get_subdirectories(parent_id) {
+            if subdir.name == subdir_name {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    pub fn get_subdirectory(
+        &self,
+        parent_id: FSID,
+        subdir_name: &str,
+    ) -> Option<(FSID, &AOCDirectory)> {
+        for (id, subdir) in self.get_subdirectories(parent_id) {
+            if subdir.name == subdir_name {
+                return Some((id, subdir));
+            }
+        }
+
+        None
+    }
+}
+
+fn print_directory(file_system: &FileSystem, dir_id: FSID, indent: usize) {
+    let dir = file_system.get_directory(dir_id);
+    println!("{:indent$}- {} (dir)", "", dir.name, indent = indent);
+
+    for id in dir.elements.iter() {
+        match file_system.get(*id) {
+            FSElement::Directory(_) => print_directory(file_system, *id, indent + 4),
+            FSElement::File(f) => println!(
+                "{:indent$}- {} (file, size={})",
+                "",
+                f.name,
+                f.size,
+                indent = indent + 4
+            ),
         }
     }
 }
@@ -37,42 +169,60 @@ fn print_directory(dir: &AOCDirectory, indent: usize) {
 fn main() -> io::Result<()> {
     let file = File::open("input.txt")?;
     let lines = BufReader::new(file).lines().map(|l| l.unwrap());
-    let mut root = Box::new(AOCDirectory {
-        name: String::from("/"),
-        elements: HashMap::new(),
-        parent: None,
-    });
-    let mut current_dir = root.as_mut();
+    let mut file_system: FileSystem = FileSystem::new();
+    let mut current_dir_id = 0 as FSID;
 
-    for line in lines {
+    for line in lines.skip(1) {
+        //println!("{line}");
+
         let parts = line.split_whitespace().collect::<Vec<_>>();
+
         match &parts[..] {
+            ["$", "cd", ".."] => {
+                current_dir_id = file_system.get_parent_id(current_dir_id);
+            }
             ["$", "cd", dir_name] => {
                 let dir_name = String::from(*dir_name);
 
-                if dir_name != current_dir.name {
-                    if current_dir.elements.contains_key(&dir_name) {
-                        current_dir = match current_dir.elements.get_mut(&dir_name).unwrap().as_mut(){
-                          FSElement::Directory(d) => d,
-                          element => panic!("{element:?} is not a directory")  
-                        };
-                    }
-                    else {
-                        current_dir.elements.insert(dir_name.clone(), Box::new(FSElement::Directory(AOCDirectory {
-                            name: dir_name,
-                            elements: HashMap::new(),
-                            parent: Some(Box::new(*current_dir))
-                        })));
-                    }
-                }
+                current_dir_id = if file_system.has_subdirectory(current_dir_id, &dir_name) {
+                    file_system
+                        .get_subdirectory(current_dir_id, &dir_name)
+                        .unwrap()
+                        .0
+                } else {
+                    file_system.create(FSElement::Directory(AOCDirectory {
+                        name: dir_name,
+                        elements: HashSet::new(),
+                        parent: Some(current_dir_id),
+                    }))
+                };
             }
-            ["$", "ls"] => println!("List directory"),
-            ["dir", dir] => println!("Directory {dir}"),
-            [file_size, file_name] => println!("File {file_name} with size {file_size}"),
+            ["$", "ls"] => continue,
+            ["dir", dir] => {
+                file_system.create(FSElement::Directory(AOCDirectory {
+                    name: String::from(*dir),
+                    elements: HashSet::new(),
+                    parent: Some(current_dir_id),
+                }));
+            }
+            [file_size, file_name] => {
+                file_system.create(FSElement::File(AOCFile {
+                    name: String::from(*file_name),
+                    size: file_size.parse().unwrap(),
+                    parent: current_dir_id,
+                }));
+            }
             parts => panic!("Unable to parse {parts:?}"),
         }
     }
 
-    print_directory(&root, 0);
+    print_directory(&file_system, 0, 0);
+    let sum: u64 = file_system
+        .get_all_directories()
+        .iter()
+        .map(|(id, _)| file_system.get_size(*id))
+        .filter(|size| size <= &100000)
+        .sum();
+    println!("{sum}");
     Ok(())
 }
