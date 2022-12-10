13257f871713ce617fa7edca8b2af3ee8df3b7c9
Rayshard Thompson <rayshard.thompson@kensho.com>
2022-12-10 10:36:49 -0500

    day 10 - part 2

--- 2022/day10/src/main.rs
+++ 2022/day10/src/main.rs
@@ -1,7 +1,45 @@
 use std::{
+    fmt,
     fs::File,
     io::{self, BufRead, BufReader},
 };
+
+#[allow(dead_code)]
+struct CRT {
+    width: usize,
+    height: usize,
+    pixels: Vec<bool>,
+}
+
+impl CRT {
+    pub fn new(width: usize, height: usize) -> CRT {
+        CRT {
+            width,
+            height,
+            pixels: vec![false; width * height],
+        }
+    }
+
+    pub fn set_pixel(&mut self, index: usize, value: bool) {
+        self.pixels[index] = value;
+    }
+}
+
+impl fmt::Display for CRT {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let lines = self.pixels.chunks(self.width).map(|line| {
+            line.iter()
+                .map(|b| if *b { '#' } else { ' ' })
+                .collect::<String>()
+        });
+
+        for line in lines {
+            writeln!(f, "{line}")?;
+        }
+
+        Ok(())
+    }
+}
 
 fn main() -> io::Result<()> {
     let file = File::open("input.txt")?;
@@ -10,6 +48,7 @@ fn main() -> io::Result<()> {
     let mut register = 1i64;
     let mut cycle = 0;
     let mut total = 0;
+    let mut crt = CRT::new(40, 6);
 
     for line in reader.lines().map(|line| line.unwrap()) {
         let parts = line.split_whitespace().collect::<Vec<_>>();
@@ -22,15 +61,25 @@ fn main() -> io::Result<()> {
         for value in values {
             cycle += 1;
 
+            // for part 1
             if (cycle - 20) % 40 == 0 {
                 total += cycle * register;
             }
+
+            // for part 2
+            let cycle_minus_1 = cycle - 1;
+            let sprite = (register - 1, register + 1);
+            crt.set_pixel(
+                cycle_minus_1 as usize,
+                cycle_minus_1 % crt.width as i64 >= sprite.0 && cycle_minus_1 % crt.width as i64 <= sprite.1,
+            );
 
             register += value;
         }
     }
 
     println!("{total}");
+    println!("{crt}");
 
     Ok(())
 }
