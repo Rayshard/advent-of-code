f5dda0237f169da3b30a11368740aeaf87011e5d
Rayshard Thompson <rayshard.thompson@kensho.com>
2022-12-23 23:18:26 -0600

    day 19 - part 2

--- 2022/day19/src/main.rs
+++ 2022/day19/src/main.rs
@@ -12,6 +12,31 @@ struct Blueprint {
     obsidian_robot_clay_cost: usize,
     geode_robot_ore_cost: usize,
     geode_robot_obsidian_cost: usize,
+    max_needed_ore_robots: usize,
+}
+
+impl Blueprint {
+    pub fn new(
+        ore_robot_ore_cost: usize,
+        clay_robot_ore_cost: usize,
+        obsidian_robot_ore_cost: usize,
+        obsidian_robot_clay_cost: usize,
+        geode_robot_ore_cost: usize,
+        geode_robot_obsidian_cost: usize,
+    ) -> Self {
+        Self {
+            ore_robot_ore_cost,
+            clay_robot_ore_cost,
+            obsidian_robot_ore_cost,
+            obsidian_robot_clay_cost,
+            geode_robot_ore_cost,
+            geode_robot_obsidian_cost,
+            max_needed_ore_robots: ore_robot_ore_cost
+                .max(clay_robot_ore_cost)
+                .max(obsidian_robot_ore_cost)
+                .max(geode_robot_ore_cost),
+        }
+    }
 }
 
 #[derive(Clone)]
@@ -41,48 +66,16 @@ impl State {
     }
 
     pub fn possible_nexts(&self, blueprint: &Blueprint) -> Vec<State> {
-        let mut result = vec![];
-
-        if self.num_ore >= blueprint.ore_robot_ore_cost {
-            result.push(State {
-                num_ore_robots: self.num_ore_robots + 1,
-                num_ore: self.num_ore + self.num_ore_robots - blueprint.ore_robot_ore_cost,
-                num_clay_robots: self.num_clay_robots,
-                num_clay: self.num_clay + self.num_clay_robots,
-                num_obsidian_robots: self.num_obsidian_robots,
-                num_obsidian: self.num_obsidian + self.num_obsidian_robots,
-                num_geode_robots: self.num_geode_robots,
-                num_geodes: self.num_geodes + self.num_geode_robots,
-            });
-        }
-
-        if self.num_ore >= blueprint.clay_robot_ore_cost {
-            result.push(State {
-                num_ore_robots: self.num_ore_robots,
-                num_ore: self.num_ore + self.num_ore_robots - blueprint.clay_robot_ore_cost,
-                num_clay_robots: self.num_clay_robots + 1,
-                num_clay: self.num_clay + self.num_clay_robots,
-                num_obsidian_robots: self.num_obsidian_robots,
-                num_obsidian: self.num_obsidian + self.num_obsidian_robots,
-                num_geode_robots: self.num_geode_robots,
-                num_geodes: self.num_geodes + self.num_geode_robots,
-            });
-        }
-
-        if self.num_ore >= blueprint.obsidian_robot_ore_cost
-            && self.num_clay >= blueprint.obsidian_robot_clay_cost
-        {
-            result.push(State {
-                num_ore_robots: self.num_ore_robots,
-                num_ore: self.num_ore + self.num_ore_robots - blueprint.obsidian_robot_ore_cost,
-                num_clay_robots: self.num_clay_robots,
-                num_clay: self.num_clay + self.num_clay_robots - blueprint.obsidian_robot_clay_cost,
-                num_obsidian_robots: self.num_obsidian_robots + 1,
-                num_obsidian: self.num_obsidian + self.num_obsidian_robots,
-                num_geode_robots: self.num_geode_robots,
-                num_geodes: self.num_geodes + self.num_geode_robots,
-            });
-        }
+        let mut result = vec![State {
+            num_ore_robots: self.num_ore_robots,
+            num_ore: self.num_ore + self.num_ore_robots,
+            num_clay_robots: self.num_clay_robots,
+            num_clay: self.num_clay + self.num_clay_robots,
+            num_obsidian_robots: self.num_obsidian_robots,
+            num_obsidian: self.num_obsidian + self.num_obsidian_robots,
+            num_geode_robots: self.num_geode_robots,
+            num_geodes: self.num_geodes + self.num_geode_robots,
+        }];
 
         if self.num_ore >= blueprint.geode_robot_ore_cost
             && self.num_obsidian >= blueprint.geode_robot_obsidian_cost
@@ -98,19 +91,51 @@ impl State {
                 num_geode_robots: self.num_geode_robots + 1,
                 num_geodes: self.num_geodes + self.num_geode_robots,
             });
-        }
+        } else if self.num_ore >= blueprint.obsidian_robot_ore_cost
+            && self.num_clay >= blueprint.obsidian_robot_clay_cost && self.num_obsidian_robots < blueprint.geode_robot_obsidian_cost
+        {
+                result.push(State {
+                    num_ore_robots: self.num_ore_robots,
+                    num_ore: self.num_ore + self.num_ore_robots - blueprint.obsidian_robot_ore_cost,
+                    num_clay_robots: self.num_clay_robots,
+                    num_clay: self.num_clay + self.num_clay_robots
+                        - blueprint.obsidian_robot_clay_cost,
+                    num_obsidian_robots: self.num_obsidian_robots + 1,
+                    num_obsidian: self.num_obsidian + self.num_obsidian_robots,
+                    num_geode_robots: self.num_geode_robots,
+                    num_geodes: self.num_geodes + self.num_geode_robots,
+                });
+        } else {
+            if self.num_ore >= blueprint.clay_robot_ore_cost
+                && self.num_clay_robots < blueprint.obsidian_robot_clay_cost
+            {
+                result.push(State {
+                    num_ore_robots: self.num_ore_robots,
+                    num_ore: self.num_ore + self.num_ore_robots - blueprint.clay_robot_ore_cost,
+                    num_clay_robots: self.num_clay_robots + 1,
+                    num_clay: self.num_clay + self.num_clay_robots,
+                    num_obsidian_robots: self.num_obsidian_robots,
+                    num_obsidian: self.num_obsidian + self.num_obsidian_robots,
+                    num_geode_robots: self.num_geode_robots,
+                    num_geodes: self.num_geodes + self.num_geode_robots,
+                });
+            }
 
-        // No consumption state
-        result.push(State {
-            num_ore_robots: self.num_ore_robots,
-            num_ore: self.num_ore + self.num_ore_robots,
-            num_clay_robots: self.num_clay_robots,
-            num_clay: self.num_clay + self.num_clay_robots,
-            num_obsidian_robots: self.num_obsidian_robots,
-            num_obsidian: self.num_obsidian + self.num_obsidian_robots,
-            num_geode_robots: self.num_geode_robots,
-            num_geodes: self.num_geodes + self.num_geode_robots,
-        });
+            if self.num_ore >= blueprint.ore_robot_ore_cost
+                && self.num_ore_robots < blueprint.max_needed_ore_robots
+            {
+                result.push(State {
+                    num_ore_robots: self.num_ore_robots + 1,
+                    num_ore: self.num_ore + self.num_ore_robots - blueprint.ore_robot_ore_cost,
+                    num_clay_robots: self.num_clay_robots,
+                    num_clay: self.num_clay + self.num_clay_robots,
+                    num_obsidian_robots: self.num_obsidian_robots,
+                    num_obsidian: self.num_obsidian + self.num_obsidian_robots,
+                    num_geode_robots: self.num_geode_robots,
+                    num_geodes: self.num_geodes + self.num_geode_robots,
+                });
+            }
+        }
 
         result
     }
@@ -119,15 +144,12 @@ impl State {
 fn determine_max_number_geodes(blueprint: &Blueprint, allowed_minutes: usize) -> usize {
     let mut states = vec![State::new()];
 
-    for minute in 1..=allowed_minutes - 2 {
-        println!("{minute}, {}", states.len());
+    for _ in 1..=allowed_minutes - 2 {
         states = states
             .iter()
             .flat_map(|state| state.possible_nexts(blueprint))
             .collect();
     }
-
-    println!("Final: {}", states.len());
 
     let best = states
         .par_iter()
@@ -149,25 +171,38 @@ fn main() -> io::Result<()> {
     let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
 
-    let result = reader
+    let blueprints = reader
         .lines()
-        .enumerate()
-        .map(|(i, line)| {
+        .map(|line| {
             let line = line.unwrap();
             let parts = line.split_whitespace().collect::<Vec<_>>();
 
-            let blueprint = Blueprint {
-                ore_robot_ore_cost: parts[6].parse().unwrap(),
-                clay_robot_ore_cost: parts[12].parse().unwrap(),
-                obsidian_robot_ore_cost: parts[18].parse().unwrap(),
-                obsidian_robot_clay_cost: parts[21].parse().unwrap(),
-                geode_robot_ore_cost: parts[27].parse().unwrap(),
-                geode_robot_obsidian_cost: parts[30].parse().unwrap(),
-            };
-
-            (i + 1) * determine_max_number_geodes(&blueprint, 24) as usize
+            Blueprint::new(
+                parts[6].parse().unwrap(),
+                parts[12].parse().unwrap(),
+                parts[18].parse().unwrap(),
+                parts[21].parse().unwrap(),
+                parts[27].parse().unwrap(),
+                parts[30].parse().unwrap(),
+            )
         })
+        .collect::<Vec<_>>();
+
+    // Part 1
+    let result = blueprints
+        .iter()
+        .enumerate()
+        .map(|(i, blueprint)| (i + 1) * determine_max_number_geodes(&blueprint, 24) as usize)
         .sum::<usize>();
+
+    println!("{result}");
+
+    // Part 2
+    let result = blueprints
+        .iter()
+        .take(3)
+        .map(|blueprint| determine_max_number_geodes(&blueprint, 32))
+        .product::<usize>();
 
     println!("{result}");
 
