013ac5a956db1c750b1348b6f6fe93c1bdb41c98
Rayshard Thompson <rayshard.thompson@kensho.com>
2023-12-04 21:22:33 -0500

    2023: day 4 part 2

--- 2023/day4/main.py
+++ 2023/day4/main.py
@@ -7,15 +7,15 @@ def part1and2(file: TextIOWrapper) -> int:
     total = defaultdict(int)
 
     for i, line in enumerate(file):
-        total[i + 1] += 1
+        total[i] += 1
 
         count = len(set.intersection(*[set(int(number.strip()) for number in cards.split(" ") if number != "") for cards in line.split(": ", 1)[1].split(" | ")]))
 
         if count != 0:
             part1_result += 2**(count - 1)
 
-            for index in range(count):
-                total[i + 2 + index] += total[i + 1]
+            for index in range(1, count + 1):
+                total[i + index] += total[i]
 
     return part1_result, sum(total.values())
 
