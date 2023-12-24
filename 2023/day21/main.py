d5d326d79d03e66bb88e03db6cbacc0342100003
Rayshard Thompson <rayshard.thompson@kensho.com>
2023-12-24 18:13:35 -0500

    2023: day 24 part 1

--- 2023/day21/main.py
+++ 2023/day21/main.py
@@ -1,14 +1,16 @@
+from collections import defaultdict
 from io import TextIOWrapper
 
 
-def print_map(garden_plots: set[tuple[int, int]], possible_positions: set[tuple[int, int]], start: tuple[int, int], max_row: int, max_col: int) -> None:
+def print_map(garden_plots: set[tuple[int, int]], possible_positions: dict[tuple[int, int], int], start: tuple[int, int], max_row: int, max_col: int) -> None:
     for row in range(0, max_row + 1):
         line = ""
 
         for col in range(0, max_col + 1):
             if (row, col) in garden_plots:
                 if (row, col) in possible_positions:
-                    line += "O"
+                    assert possible_positions[(row, col)] > 0
+                    line += str(possible_positions[(row, col)])
                 elif (row, col) == start:
                     line += "S"
                 else:
@@ -35,13 +37,15 @@ def part1(file: TextIOWrapper) -> int:
 
         max_row, max_col = row, col
 
-    possible_positions = {start}
+    max_row_plus_1, max_col_plus_1 = max_row + 1, max_col + 1
+    possible_positions = {start: 1}
 
-    for _ in range(64):
-        next_pps = set[tuple[int, int]]()
+    for _ in range(10):
+        next_pps = defaultdict[tuple[int, int], int](int)
+        next_pps.update(possible_positions)
 
-        while possible_positions:
-            (row, col) = possible_positions.pop()
+        for (row, col), count in possible_positions.items():
+            assert count != 0
             adj = [
                 (row + 1, col),
                 (row - 1, col),
@@ -50,14 +54,21 @@ def part1(file: TextIOWrapper) -> int:
             ]
 
             for a_row, a_col in adj:
+                a_row_modded, a_col_modded = a_row % max_row_plus_1, a_col % max_col_plus_1
                 if a_row < 0 or a_row > max_row or a_col < 0 or a_col > max_col:
-                    continue
+                    next_pps[(a_row_modded, a_col_modded)] += 1
                 elif (a_row, a_col) in garden_plots:
-                    next_pps.add((a_row, a_col))
+                    next_pps[(a_row_modded, a_col_modded)] = 1
+
+            next_pps[(row, col)] -= 1
+            if next_pps[(row, col)] == 0:
+                next_pps.pop((row, col))
 
         possible_positions = next_pps
+        print_map(garden_plots, possible_positions, start, max_row, max_col)
+        print()
 
-    return len(possible_positions)
+    return sum(possible_positions.values())
 
 
 
---
+++ 2023/day24/main.py
@@ -0,0 +1,62 @@
+from dataclasses import dataclass
+from io import TextIOWrapper
+import re
+from itertools import combinations
+from typing import Optional
+
+@dataclass
+class Hailstone:
+    x: int
+    y: int
+    z: int
+    vx: int
+    vy: int
+    vz: int
+
+    @property
+    def slope(self) -> float:
+        return self.vy / self.vx
+
+    def y_at_x(self, x: float) -> float:
+        return self.slope * (x - self.x) + self.y
+
+    def __repr__(self) -> str:
+        return f"{self.x}, {self.y}, {self.z} @ {self.vx}, {self.vy}, {self.vz}"
+
+
+def calculate_x_intersection(hailstone_a: Hailstone, hailstone_b: Hailstone) -> Optional[float]:
+    if hailstone_a.slope == hailstone_b.slope:
+        return None
+
+    return (hailstone_b.y - hailstone_a.y + hailstone_a.slope * hailstone_a.x - hailstone_b.slope * hailstone_b.x) / (hailstone_a.slope - hailstone_b.slope)
+
+
+def cross_inside_test_area(hailstone_a: Hailstone, hailstone_b: Hailstone, test_area_boundary: tuple[int, int]) -> bool:
+    if (x_int := calculate_x_intersection(hailstone_a, hailstone_b)) is None:
+        return False
+
+    t_a = (x_int - hailstone_a.x) / hailstone_a.vx
+    t_b = (x_int - hailstone_b.x) / hailstone_b.vx
+
+    if t_a < 0 or t_b < 0:
+        return False
+
+    y_int = hailstone_a.y_at_x(x_int)
+
+    test_area_min, test_area_max = test_area_boundary
+    return (test_area_min <= x_int <= test_area_max) and (test_area_min <= y_int <= test_area_max)
+
+def part1(file: TextIOWrapper) -> int:
+    hailstones = list[Hailstone]()
+    test_area_boundary = (200000000000000, 400000000000000)
+
+    for line in file:
+        x, y, z, vx, vy, vz = (int(i) for i in re.split(r",?\s+@?\s*", line.strip()))
+        hailstones.append(Hailstone(x, y, z, vx, vy, vz))
+
+    return sum(cross_inside_test_area(hailstone_a, hailstone_b, test_area_boundary) for hailstone_a, hailstone_b in combinations(hailstones, 2))
+
+
+if __name__ == "__main__":
+    with open("input.txt") as file:
+        print(part1(file))
