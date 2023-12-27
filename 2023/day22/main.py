12f815889f2161b29217763f757936ee2579dadb
Rayshard Thompson <rayshard.thompson@kensho.com>
2023-12-26 20:30:21 -0500

    2023: day22 part 1

--- 2023/day22/main.py
+++ 2023/day22/main.py
@@ -1,133 +1,103 @@
 from collections import defaultdict
 from io import TextIOWrapper
-from pprint import pprint as print, pformat as format
 
 
 Coord = tuple[int, int, int]
-Coord2D = tuple[int, int]
+Block = list[Coord]
 
 
-class Block:
-    def __init__(self, start: Coord, end: Coord) -> None:
-        (x1, y1, z1) = self.start = start
-        (x2, y2, z2) = self.end = end
-
-        assert x1 >= x2
-        assert y1 >= y2
-        assert z1 >= z2
-
-    @property
-    def min_z(self) -> int:
-        self.start[2]
-
-    @property
-    def max_z(self) -> int:
-        self.end[2]
-
-    def __repr__(self) -> str:
-        return format((self.start, self.end))
-
-
-class Space:
-    def __init__(self) -> None:
-        self.blocks = defaultdict[int, dict[int, Block]](dict[int, Block])
-        self.__max_z = 0
-        self.__next_block_id = 0
-
-    @property
-    def max_z(self) -> int:
-        return self.__max_z
-
-    def add_block(self, block: Block) -> None:
-        for z in range(block.min_z, block.max_z + 1):
-            self.blocks[z][self.__next_block_id] = block
-
-        self.__next_block_id += 1
-        self.__max_z = max(self.__max_z, block.max_z)
-
-    def __repr__(self) -> str:
-        return format(self.blocks)
-
-
-def parse_space(file: TextIOWrapper) -> Space:
-    space = Space()
+def parse_space(file: TextIOWrapper) -> tuple[set[Coord], list[Block]]:
+    space = set[Coord]()
+    blocks = list[Block]()
 
     for line in file:
-        start, end = [tuple(map(int, item.split(","))) for item in line.strip().split("~")]
-        space.add_block(Block(start, end))
+        (x1, y1, z1), (x2, y2, z2) = [tuple(map(int, item.split(","))) for item in line.strip().split("~")]
 
-    return space
+        bricks = list[Coord]()
+
+        for x in range(x1, x2 + 1):
+            for y in range(y1, y2 + 1):
+                for z in range(z1, z2 + 1):
+                    bricks.append((x, y, z))
+
+        space.update(bricks)
+        blocks.append(bricks)
+
+    return space, blocks
 
 
-def part1(file: TextIOWrapper) -> int:
-    space = parse_space(file)
-    print(space)
+def simulate(space: set[Coord], blocks: list[Block]) -> tuple[set[Coord], list[Block], set[int]]:
+    fallen_blocks = set[int]()
 
-    iterations = 0
+
     while True:
-        print((iterations := iterations + 1))
-        new_space = Space()
+        old_space = space.copy()
 
-        for block in space.blocks[1].values():
-            new_space.add_block(block)
+        for i in range(len(blocks)):
+            block = blocks[i]
+            space.difference_update(block)
+            done = False
 
-        changed = False
-        handled = set[int]()
+            while not done:
+                new_block = list[Coord]()
+                for x, y, z in block:
+                    new_coord = (x, y, z - 1)
 
-        for z in range(2, space.max_z + 1):
-            current_plane = space.blocks[z]
-            blocks_beneath = new_space.blocks[z - 1].values()
+                    if z == 1 or new_coord in space:
+                        done = True
+                        new_block = block
+                        break
+                    else:
+                        new_block.append(new_coord)
+                        fallen_blocks.add(i)
 
-            for block_id, block in current_plane.items():
-                if block_id in handled:
-                    continue
-                else:
-                    handled.add(block_id)
+                block = new_block
 
-                if block.bottom.intersection(plane_beneath):
-                    new_space.add_block(block)
-                else:
-                    new_space.add_block(Block({(x, y, z - 1) for (x, y, z) in block.bricks}))
-                    changed = True
+            space.update(block)
+            blocks[i] = block
 
-        space = new_space
-
-        if not changed:
+        if old_space == space:
             break
 
-    handled = set(space.blocks[space.max_z].keys())
-    can_disintegrate = len(space.blocks[space.max_z].keys())
+    return space, blocks, fallen_blocks
 
-    supports = defaultdict[int, set[int]](set[int])
-    supported = defaultdict[int, set[int]](set[int])
 
-    for z in range(1, space.max_z):
-        current_plane = space.blocks[z]
-        blocks_above = new_space.blocks[z + 1]
+def find_disintegratable_blocks(blocks: list[Block]) -> set[int]:
+    brick_to_blocks = dict[Coord, int]()
 
-        for block_id, block in current_plane.items():
-            if block_id in handled:
-                continue
-            else:
-                handled.add(block_id)
+    for block, bricks in enumerate(blocks):
+        for brick in bricks:
+            brick_to_blocks[brick] = block
 
-            for block_above_id, block_above in blocks_above.items():
-                if block.top.intersection(block_above.bottom):
-                    supports[block_id].add(block_above_id)
-                    supported[block_above_id].add(block_id)
 
-    for block_id, supported_block_ids in supports.items():
-        can_be_disintergrated = True
+    block_to_supporting = defaultdict[int, set[int]](set[int])
+    block_to_supported_by = defaultdict[int, set[int]](set[int])
 
-        for supported_block_id in supported_block_ids:
-            if len(supported[supported_block_id]) < 2:
-                can_be_disintergrated = False
-                break
+    for brick, block in brick_to_blocks.items():
+        x, y, z = brick
+        above = (x, y, z + 1)
 
-        if can_be_disintergrated:
-            can_disintegrate += 1
+        if above in brick_to_blocks:
+            supported = brick_to_blocks[above]
 
-    return can_disintegrate
+            if supported != block:
+                block_to_supporting[block].add(supported)
+                block_to_supported_by[supported].add(block)
+
+    disintegratable_blocks = set(range(len(blocks)))
+
+    for supporters in block_to_supported_by.values():
+        if len(supporters) == 1:
+            disintegratable_blocks.difference_update(supporters)
+
+    return disintegratable_blocks
+
+def part1(file: TextIOWrapper) -> int:
+    space, blocks = parse_space(file)
+    space, blocks, _ = simulate(space, blocks)
+    disintegratable_blocks = find_disintegratable_blocks(blocks)
+
+    return len(disintegratable_blocks)
 
 
 if __name__ == "__main__":
