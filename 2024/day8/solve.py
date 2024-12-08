984447c439216d6b50e374b111ee4447504761a9
Rayshard Thompson <rayshard.t123@gmail.com>
2024-12-08 09:54:44 -0500

    2024: day 8 part 2

--- 2024/day8/solve.py
+++ 2024/day8/solve.py
@@ -7,7 +7,26 @@ from typing import Mapping
 Vec2 = tuple[int, int]
 
 
-def find_antinodes(map: Mapping[str, set[Vec2]], map_size: int) -> set[Vec2]:
+def parse_map(input: TextIOWrapper) -> tuple[Mapping[str, set[Vec2]], int]:
+    map = defaultdict[str, set[Vec2]](set[Vec2])
+    map_size = 0
+
+    for row, line in enumerate(input):
+        line = line.strip()
+        map_size = max(map_size, len(line))
+
+        for col, char in enumerate(line):
+            if char == ".":
+                continue
+
+            map[char].add((row, col))
+
+    return map, map_size
+
+
+def find_antinodes(
+    map: Mapping[str, set[Vec2]], map_size: int, max_reps: int = 1
+) -> set[Vec2]:
     pairs = set[tuple[Vec2, Vec2]]()
     antinodes = set[tuple[Vec2]]()
 
@@ -24,14 +43,22 @@ def find_antinodes(map: Mapping[str, set[Vec2]], map_size: int) -> set[Vec2]:
                     continue
 
                 row_diff, col_diff = row2 - row1, col2 - col1
-                antinode1 = (row1 - row_diff, col1 - col_diff)
-                antinode2 = (row2 + row_diff, col2 + col_diff)
 
-                if (0 <= antinode1[0] < map_size) and (0 <= antinode1[1] < map_size):
-                    antinodes.add(antinode1)
+                for i in range(1, max_reps + 1):
+                    antinode = (row1 - row_diff * i, col1 - col_diff * i)
 
-                if (0 <= antinode2[0] < map_size) and (0 <= antinode2[1] < map_size):
-                    antinodes.add(antinode2)
+                    if (0 <= antinode[0] < map_size) and (0 <= antinode[1] < map_size):
+                        antinodes.add(antinode)
+                    else:
+                        break
+
+                for i in range(1, max_reps + 1):
+                    antinode = (row2 + row_diff * i, col2 + col_diff * i)
+
+                    if (0 <= antinode[0] < map_size) and (0 <= antinode[1] < map_size):
+                        antinodes.add(antinode)
+                    else:
+                        break
 
                 pairs.add((antenna1, antenna2))
 
@@ -39,24 +66,14 @@ def find_antinodes(map: Mapping[str, set[Vec2]], map_size: int) -> set[Vec2]:
 
 
 def part1(input: TextIOWrapper) -> int:
-    map = defaultdict[str, set[Vec2]](set[Vec2])
-    map_size = 0
-
-    for row, line in enumerate(input):
-        line = line.strip()
-        map_size = max(map_size, len(line))
-
-        for col, char in enumerate(line):
-            if char == ".":
-                continue
-
-            map[char].add((row, col))
-
+    map, map_size = parse_map(input)
     return len(find_antinodes(map, map_size))
 
 
 def part2(input: TextIOWrapper) -> int:
-    return 0
+    map, map_size = parse_map(input)
+    antinodes = find_antinodes(map, map_size, max_reps=map_size * map_size)
+    return len(antinodes.union(*map.values()))
 
 
 if __name__ == "__main__":
