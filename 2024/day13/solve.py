ece8000c103aea74bcbf4280d65825cc61ee26c1
Rayshard Thompson <rayshard.t123@gmail.com>
2024-12-17 21:56:22 -0500

    day 13: part 2

--- 2024/day13/solve.py
+++ 2024/day13/solve.py
@@ -1,9 +1,7 @@
-from collections import defaultdict
-from dataclasses import dataclass, field
 from io import TextIOWrapper
 import sys
 import string
-import math
+from typing import Optional
 
 Vec2 = tuple[int, int]
 ClawMachine = tuple[Vec2, Vec2, Vec2]
@@ -31,31 +29,42 @@ def parse_claw_machines(input: TextIOWrapper) -> list[ClawMachine]:
     return claw_machines
 
 
+def solve(button_a: Vec2, button_b: Vec2, prize: Vec2, error) -> Optional[int]:
+    Ax, Ay = button_a
+    Bx, By = button_b
+    Px, Py = prize
+
+    b: float = (Px - Py * (Ax / Ay)) / (Bx - By * (Ax / Ay))
+    a: float = (Px / Ax) - ((Bx * b) / Ax)
+
+    a_rounded, b_rounded = round(a), round(b)
+
+    if abs(a - a_rounded) <= error and abs(b - b_rounded) <= error:
+        a, b = a_rounded, b_rounded
+
+        if a >= 0 and b >= 0:
+            return a * 3 + b
+
+    return None
+
+
 def part1(input: TextIOWrapper) -> int:
-    claw_machines = parse_claw_machines(input)
-    result = 0
-
-    for button_a, button_b, prize in claw_machines:
-        Ax, Ay = button_a
-        Bx, By = button_b
-        Px, Py = prize
-
-        b: float = (Px - (Py * Ax) / Ay) / (Bx - (By * Ax) / Ay)
-        a: float = (Px - (Bx * b)) / Ax
-
-        a_rounded, b_rounded = round(a), round(b)
-
-        if math.isclose(a, a_rounded) and math.isclose(b, b_rounded):
-            a, b = a_rounded, b_rounded
-
-            if (0 <= a <= 100) and (0 <= b <= 100):
-                result += a * 3 + b
-
-    return result
+    return sum(
+        tokens
+        for button_a, button_b, prize in parse_claw_machines(input)
+        if (tokens := solve(button_a, button_b, prize, error=1e-9)) is not None
+    )
 
 
 def part2(input: TextIOWrapper) -> int:
-    return 0
+    add = 10000000000000
+
+    return sum(
+        tokens
+        for button_a, button_b, (Px, Py) in parse_claw_machines(input)
+        if (tokens := solve(button_a, button_b, (Px + add, Py + add), error=1e-3))
+        is not None
+    )
 
 
 if __name__ == "__main__":
