b662cd3473fb36e288345d4988936c8645b30dcb
Rayshard Thompson <rayshard.t123@gmail.com>
2024-12-20 09:28:11 -0500

    day 19: part 2

--- 2024/day19/solve.py
+++ 2024/day19/solve.py
@@ -3,34 +3,54 @@ import sys
 
 
 def parse_input(input: TextIOWrapper) -> tuple[set[str], list[str]]:
-    patterns = set(input.readline().split(", "))
+    patterns = set(input.readline().strip().split(", "))
     input.readline()
     designs = [line.strip() for line in input.readlines()]
 
     return patterns, designs
 
 
-def can_make_design(patterns: set[str], design: str) -> bool:
+def num_ways_to_make_design(
+    patterns: set[str], design: str, cache: dict[str, int]
+) -> int:
+    if design in cache:
+        return cache[design]
+
+    count = 0
+
     for size in reversed(range(len(design))):
-        if design[:size + 1] not in patterns:
+        if design[: size + 1] not in patterns:
             continue
 
-        remaining = design[size + 1:]
+        remaining = design[size + 1 :]
 
-        if not remaining or can_make_design(patterns, remaining):
-            return True
+        if not remaining:
+            count += 1
+        else:
+            count += num_ways_to_make_design(patterns, remaining, cache)
 
-    return False
+    cache[design] = count
+    return count
 
+
+def can_make_design(patterns: set[str], design: str, cache: dict[str, int]) -> bool:
+    # This is less efficient than stopping once a design is determined
+    # can be made but hey
+    return num_ways_to_make_design(patterns, design, cache) != 0
 
 
 def part1(input: TextIOWrapper) -> int:
     patterns, designs = parse_input(input)
-    return sum(can_make_design(patterns, design) for design in designs)
+    cache = dict[str, int]()
+
+    return sum(can_make_design(patterns, design, cache) for design in designs)
 
 
 def part2(input: TextIOWrapper) -> int:
-    return 0
+    patterns, designs = parse_input(input)
+    cache = dict[str, int]()
+
+    return sum(num_ways_to_make_design(patterns, design, cache) for design in designs)
 
 
 if __name__ == "__main__":
